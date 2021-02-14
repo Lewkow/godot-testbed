d881155b6e04e845ca7e39538aa3bb20671608e3
Nick Lewkow <nlewkow@gmail.com>
2021-02-14 08:22:05 -0500

    init commit

---
+++ Astroid-Tutorial/characters/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const SPEED := 600
+
+func _physics_process(delta: float) -> void:
+	var velocity := Vector2()
+
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x = -SPEED
+	if (Input.is_action_pressed("ui_right")):
+		velocity.x = SPEED
+	if (Input.is_action_pressed("ui_up")):
+		velocity.y = -SPEED
+	if (Input.is_action_pressed("ui_down")):
+		velocity.y = SPEED
+
+	move_and_collide(velocity * delta)
+
+func _unhandled_key_input(event: InputEventKey) -> void:
+	if (event.is_action_pressed("shoot")):
+		$LaserWeapon.shoot()
