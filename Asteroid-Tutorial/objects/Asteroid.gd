2898c1cff494e4728bd730f65df8b730d7bd5110
Nick Lewkow <nick@yakalabs.com>
2021-03-13 11:28:45 -0500

    collisions, lasers working

---
+++ Asteroid-Tutorial/objects/Asteroid.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+var is_exploded := false
+
+func explode():
+	if is_exploded:
+		return
+
+	is_exploded = true
+
+	get_parent().remove_child(self)
+	queue_free()
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	queue_free()
--- Asteroid-Tutorial/objects/Laser.gd
+++ Asteroid-Tutorial/objects/Laser.gd
@@ -8,3 +8,10 @@ func _process(delta: float) -> void:
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	queue_free()
+
+
+func _on_Laser_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
+	if (!self.is_queued_for_deletion() && body.is_in_group("asteroids")):
+		body.call_deferred("explode")
+		get_parent().remove_child(self)
+		queue_free()
