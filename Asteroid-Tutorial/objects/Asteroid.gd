4e69f18f873a57a41a16e15d0efa0703957a2f32
Nick <nick@nicks-mbp.lan>
2021-03-13 19:39:23 -0500

    add particles and sounds to explosions

--- Asteroid-Tutorial/objects/Asteroid.gd
+++ Asteroid-Tutorial/objects/Asteroid.gd
@@ -3,18 +3,35 @@ extends RigidBody2D
 signal explode
 
 var asteroid_small_scene := load("res://objects/AsteroidSmall.tscn")
+var explosion_particles_scene := load("res://objects/ParticlesAsteroidExplosion.tscn")
 var rng = RandomNumberGenerator.new()
 var is_exploded := false
 
 func _ready() -> void:
 	var main_camera = get_node("/root/Game/MainCamera")
 	self.connect("explode", main_camera, "asteroid_exploded")
+	
+func _play_explosion_sound():
+	var explosion_sound = AudioStreamPlayer2D.new()
+	explosion_sound.stream = load("res://assets/audio/sfx/AsteroidExplosion.wav")
+	explosion_sound.pitch_scale = 1
+	explosion_sound.position = self.position
+	get_parent().add_child(explosion_sound)
+	explosion_sound.play(0)
+	
+func _explosion_particles():
+	var explosion_particles = explosion_particles_scene.instance()
+	explosion_particles.position = self.position
+	get_parent().add_child(explosion_particles)
+	explosion_particles.emitting = true
 
 func explode():
 	if is_exploded:
 		return
 
 	is_exploded = true
+	_explosion_particles()
+	_play_explosion_sound()
 	emit_signal("explode")
 	_spawn_asteroid_smalls(4)
 	get_parent().remove_child(self)
