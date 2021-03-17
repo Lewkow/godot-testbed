a655292e8b5d6d10f20ed2bc433a8f36a705c970
Nick Lewkow <nick@yakalabs.com>
2021-03-17 19:20:50 -0400

    add twins update

--- Creeps-Tutorial/Player.gd
+++ Creeps-Tutorial/Player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
@@ -26,4 +29,24 @@ func _process(delta):
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
 
--- Twins-POC/characters/Player.gd
+++ Twins-POC/characters/Player.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
 
-const SPEED := 600
+const SPEED := 1
+var velocity := Vector2()
 
 func _physics_process(delta: float) -> void:
-	var velocity := Vector2()
 
 	if (Input.is_action_pressed("ui_left")):
-		velocity.x = -SPEED
+		velocity.x += -SPEED
 	if (Input.is_action_pressed("ui_right")):
-		velocity.x = SPEED
+		velocity.x += SPEED
 	if (Input.is_action_pressed("ui_up")):
-		velocity.y = -SPEED
+		velocity.y += -SPEED
 	if (Input.is_action_pressed("ui_down")):
-		velocity.y = SPEED
+		velocity.y += SPEED
 
 	move_and_collide(velocity * delta)
 	
