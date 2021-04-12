9df2043ce1bf3b540b9a3db8ad5007959cacae53
Nick Lewkow <nlewkow@gmail.com>
2021-04-12 09:20:07 -0400

    update free fall

--- Free-Fall/PlatformGenerator.gd
+++ Free-Fall/PlatformGenerator.gd
@@ -10,12 +10,11 @@ func platform_engine():
 	var player = get_parent().get_node("Player")
 	var player_position = player.position
 	var player_future = player_position.y + get_parent().get_viewport().size.y
-	var chunk = 773
+	var random_percentage = rng.randf_range(0.0, 100.0)
 	var tol = 10
-	var future_mod = fmod(player_future, chunk)
 	
 	# generate platform in future
-	if future_mod <= tol:
+	if random_percentage <= tol:
 		var randx = rng.randf_range(0.0, get_parent().get_viewport().size.x)
 		var new_platform = Platform.instance()
 		new_platform.position = Vector2(randx, player_future)
