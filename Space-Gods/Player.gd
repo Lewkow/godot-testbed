8a32ebace495ea2e95f15dfbd8e1f56b0a05b832
Nick Lewkow <nick@yakalabs.com>
2021-04-22 17:28:09 -0400

    update auto logic

--- Space-Gods/Player.gd
+++ Space-Gods/Player.gd
@@ -4,75 +4,89 @@ export (int) var engine_thrust
 export (int) var spin_thrust
 
 var thrust = Vector2()
+var thrust_min = 100
 var rotation_dir = 0.0
 var screensize
 var zoom_up_lim = 0.1
 var zoom_down_lim = 0.75
 var zoom_diff = 0.05
-var angular_velocity_lim = 5
+var angular_velocity_lim = 1
 var linear_velocity_lim = 10
+var linear_velocity_min_lim = 1
 var auto_destination = Vector2()
-var auto_yaw_rad = 0.0
+var auto_destination_piece
 var auto_yaw_on = false
+var auto_yaw_dot = 0
 var auto_thrust_on = false
 
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
+	if (event is InputEventMouseButton) and (event.is_pressed()):
 		move_to_this_position(event.position)
-#	if event in InputEventMouseButton and event.is_pressed():
-#		if event.button_index == BUTTON_WHEEL_UP:
-#			if $Camera2D.zoom > Vector2(zoom_up_lim, zoom_up_lim):
-#				$Camera2D.zoom = $Camera2D.zoom-Vector2(zoom_diff, zoom_diff)
-#		elif event.button_index == BUTTON_WHEEL_DOWN:
-#			if $Camera2D.zoom < Vector2(zoom_down_lim, zoom_down_lim):
-#				$Camera2D.zoom = $Camera2D.zoom-Vector2(zoom_diff, zoom_diff)
+	elif (event in InputEventMouseButton) and (event.is_pressed()):
+		if event.button_index == BUTTON_WHEEL_UP:
+			print("wheel up")
+			if $Camera2D.zoom > Vector2(zoom_up_lim, zoom_up_lim):
+				$Camera2D.zoom = $Camera2D.zoom-Vector2(zoom_diff, zoom_diff)
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			if $Camera2D.zoom < Vector2(zoom_down_lim, zoom_down_lim):
+				$Camera2D.zoom = $Camera2D.zoom-Vector2(zoom_diff, zoom_diff)
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
 
 func check_auto_yaw():
+	var unit_dir = Vector2.UP.rotated(rotation)
 	var move_path = auto_destination - position
-	var angle_diff = move_path.angle_to(Vector2.UP.rotated(rotation))
-	if abs(angle_diff) < 0.01:
+	var position_dot = unit_dir.dot(move_path.normalized())
+	var angle_diff = move_path.angle_to(unit_dir)
+	if position_dot >= 0.99:
 		angular_velocity = 0
 		auto_yaw_on = false
+		if move_path.length() > 10:
+			auto_thrust_on = true
 	else:
-		if angle_diff < 0 and abs(angular_velocity) < angular_velocity_lim:
-			# rotate CW
+		if (angle_diff < 0) and (abs(angular_velocity) < angular_velocity_lim):
 			yaw_cw()
-		elif angle_diff > 0 and abs(angular_velocity) < angular_velocity_lim:
-			# rotate CCW
+		elif (angle_diff > 0) and (abs(angular_velocity) < angular_velocity_lim):
 			yaw_ccw()
 
 func check_auto_thrust():
-	var thrust_min = 100
 	var move_path = auto_destination - position
 	var move_len = move_path.length()
-	if move_len > thrust_min and abs(linear_velocity.length()) < linear_velocity_lim:
-		auto_boost()
+	if (auto_yaw_dot == 1) and \
+	   (move_len > 2*auto_destination_piece) and \
+	   (linear_velocity.length() < linear_velocity_lim):
+		boost()
+	elif (auto_yaw_dot == 1) and \
+		 (move_len < 2*auto_destination_piece):
+		auto_thrust_on = false
+		auto_yaw_dot = -1
+		auto_yaw_on = true
+	elif (auto_yaw_dot == -1) and \
+		 (linear_velocity.length() > linear_velocity_min_lim):
+		boost()
+	elif (auto_yaw_dot == -1) and (move_len < 5):
+		linear_velocity = Vector2.ZERO
+		auto_thrust_on = false
+		auto_yaw_on = false
 
 func move_to_this_position(this_position):
 	auto_destination = this_position
 	var move_path = auto_destination - position
 	var move_dist = move_path.length()
+	auto_destination_piece = move_dist / 3.0
 	var unit_dir = Vector2.UP.rotated(rotation)
 	var angle_diff = move_path.angle_to(unit_dir)
 	print("pos: ({x}, {y})".format({"x": position.x, "y": position.y}))
 	print("unt: ({x}, {y})".format({"x": unit_dir.x, "y": unit_dir.y}))
 	print("des: ({x}, {y})".format({"x": auto_destination.x, "y": auto_destination.y}))
 	print("pth: ({x}, {y})".format({"x": move_path.x, "y": move_path.y}))
+	print("dot: {d}".format({"d": unit_dir.dot(move_path.normalized())}))
 	print("ang: {d}".format({"d": angle_diff}))
-	auto_yaw_rad = angle_diff
 	auto_yaw_on = true
-	auto_thrust_on = true
-	if angle_diff < 0:
-		# rotate CW
-		yaw_cw()
-	else:
-		# rotate CCW
-		yaw_ccw()
+	auto_yaw_dot = 1.0
 
-func auto_boost():
+func boost():
 	thrust = Vector2(0, -engine_thrust)
 	$MainThrusters.emitting = true
 
@@ -86,9 +100,9 @@ func yaw_ccw():
 	$YawLeftCCW.emitting = true
 	$YawRightCCW.emitting = true
 
-func get_input():
+func get_manual_input():
 	if Input.is_action_pressed("ui_up"):
-		auto_boost()
+		boost()
 	elif Input.is_action_pressed("ui_right"):
 		yaw_cw()
 	elif Input.is_action_pressed("ui_left"):
@@ -103,7 +117,7 @@ func get_input():
 
 func _process(delta):
 	rotation_dir = 0
-	get_input()
+	get_manual_input()
 	if auto_yaw_on:
 		check_auto_yaw()
 	elif auto_thrust_on:
